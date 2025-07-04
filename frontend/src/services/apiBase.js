559ed67ad2941e0107a9b697a3336021831d5403
esthephany <echoquehuancal@ulasalle.edu.pe>
2025-07-03 22:20:06 -0500

    versiones

--- frontend/src/services/apiBase.js
+++ frontend/src/services/apiBase.js
@@ -1,6 +1,6 @@
 const API_BASE_URL =
   window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
     ? 'http://localhost:3000/api'
-    : 'https://TU_DOMINIO_BACKEND/api'; // Cambia TU_DOMINIO_BACKEND por tu dominio real
+    : 'https://cds-final-fabianatura.onrender.com/api'; //DOMINIO_BACKEND 
 
 export default API_BASE_URL;
